61299225bab63517fe635061d8b4dd145de33718
Arthur404 <ananarben@gmail.com>
2017-03-15 15:08:58 +0200

    mailcimp added

--- wp-content/themes/dwellings/footer.php
+++ wp-content/themes/dwellings/footer.php
@@ -14,11 +14,39 @@
 	</main><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dwellings' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'dwellings' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'dwellings' ), 'dwellings', '<a href="https://automattic.com/" rel="designer">Geek Hub 2017</a>' ); ?>
-		</div><!-- .site-info -->
+        <div class="container">
+            <div class="site-info">
+
+                <div class="row">
+                    <div class="col-xs-12 col-sm-7">
+                        <?php the_custom_logo(); ?>
+                        <div class="nav-footer">
+                            <?php wp_nav_menu(array('them_location' => 'menu-1', 'container' => false, 'menu_class' => 'navigation')) ?>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-5">
+                        <label for="subscribe">Join our newsletter</label>
+                        <input id="subscribe" type="text">
+                        <input type="button" value="Subscribe">
+                    </div>
+                    <div class="col-xs-12">
+                        <div class="copy">
+                            &copy; <?php echo get_theme_mod('copy'); ?>,<?= date(' Y') ?>
+                        </div>
+                        <div class="implementers">
+                            <span class="design">
+                                Design by Maria Osadcha
+                            </span>
+                            <span class="dev">
+                                Developed by Geekhub <a href="http://geekhub.ck.ua/">geekhub.ck.ua</a>
+                            </span>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div><!-- .site-info -->
+        </div><!-- .scontainer -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
