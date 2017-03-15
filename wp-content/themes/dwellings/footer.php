2ecadd7414833ed21d3e3f130a5148d7d8adee43
Arthur404 <ananarben@gmail.com>
2017-03-15 22:22:34 +0200

    footer style added

--- wp-content/themes/dwellings/footer.php
+++ wp-content/themes/dwellings/footer.php
@@ -20,23 +20,21 @@
                 <div class="row">
                     <div class="col-xs-12 col-sm-7">
                         <?php the_custom_logo(); ?>
-                        <div class="nav-footer">
-                            <?php wp_nav_menu(array('them_location' => 'menu-1', 'container' => false, 'menu_class' => 'navigation')) ?>
-                        </div>
+                        <?php wp_nav_menu(array('them_location' => 'menu-1', 'container' => false, 'menu_class' => 'nav-footer')) ?>
                     </div>
                     <div class="col-xs-12 col-sm-5">
                         <?php dynamic_sidebar( 'sidebar-2' ); ?>
                     </div>
                     <div class="col-xs-12">
-                        <div class="copy">
-                            &copy; <?php echo get_theme_mod('copy'); ?>,<?= date(' Y') ?>
-                        </div>
+                        <span class="copy">
+                            &copy; <a href="/"><?php echo get_theme_mod('copy'); ?></a>,<?= date(' Y') ?>
+                        </span>
                         <div class="implementers">
                             <span class="design">
                                 Design by Maria Osadcha
                             </span>
                             <span class="dev">
-                                Developed by Geekhub <a href="http://geekhub.ck.ua/">geekhub.ck.ua</a>
+                                Developed by Geekhub <a href="http://geekhub.ck.ua/" target="_blank">geekhub.ck.ua</a>
                             </span>
                         </div>
 
