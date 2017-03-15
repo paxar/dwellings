84c77cd00d87bcd8f0f35dfcfc46aa3bcf3aa9fe
Pavel Charchenko <paxar81@gmail.com>
2017-03-15 15:16:45 +0200

    Add campaign pages   not work fine :-(

---
+++ wp-content/themes/dwellings/single-campaign.php
@@ -0,0 +1,24 @@
+<?php
+/*Testing page*/
+?>
+<?php get_header(); ?>
+
+<p>Single cammaign page !</p>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+        <?php
+        while ( have_posts() ) : the_post();
+
+          the_content();
+
+        endwhile; // End of the loop.
+        ?>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
--- wp-content/themes/dwellings/template-parts/content.php
+++ wp-content/themes/dwellings/template-parts/content.php
@@ -27,6 +27,7 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+
 		<?php
 			the_content( sprintf(
 				/* translators: %s: Name of current post. */
@@ -39,6 +40,8 @@
 				'after'  => '</div>',
 			) );
 		?>
+
+
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
