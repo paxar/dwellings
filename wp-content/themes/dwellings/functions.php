606f2c48eab45e3bb2f9586f429d8ab81622156d
Arthur404 <ananarben@gmail.com>
2017-03-15 11:04:27 +0200

    rename main js

--- wp-content/themes/dwellings/functions.php
+++ wp-content/themes/dwellings/functions.php
@@ -129,9 +129,9 @@ function dwellings_scripts() {
 
     wp_enqueue_script('bootstrap');
 
-    wp_register_script('main_js', get_template_directory_uri() . '/js/main.js', ['jquery'], '', true);
+    wp_register_script('home_page_js', get_template_directory_uri() . '/js/home_page.js', ['jquery'], '', true);
 
-    wp_enqueue_script('main_js');
+    wp_enqueue_script('home_page_js');
 
 }
 add_action( 'wp_enqueue_scripts', 'dwellings_scripts' );
