fa3413dfde9c7ee6027da4ec66b50df7f6288197
lyudmila-schastlyvets <zorialv@gmail.com>
2017-03-15 22:50:55 +0200

    Added data file

--- wp-content/themes/dwellings/inc/template-tags.php
+++ wp-content/themes/dwellings/inc/template-tags.php
@@ -13,9 +13,9 @@ if ( ! function_exists( 'dwellings_posted_on' ) ) :
  */
 function dwellings_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
+//	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+//		$time_string = '<time class="updated" datetime="%3$s">%4$s</time>';
+//	}
 
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
@@ -24,18 +24,17 @@ function dwellings_posted_on() {
 		esc_html( get_the_modified_date() )
 	);
 
-	$posted_on = sprintf(
-		esc_html_x( '%s', 'post date', 'dwellings' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . date( "j F, Y", strtotime(strip_tags($time_string)) ). '</a>'
-	);
+    $posted_on = sprintf(
+        esc_html_x( '%s', 'post date', 'dwellings' ),
+        '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . date( "j F, Y", strtotime(strip_tags($time_string)) ). '</a>'
+    );
 
-	$byline = sprintf(
-		esc_html_x( 'By %s', 'post author', 'dwellings' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
+    $byline = sprintf(
+        esc_html_x( 'By %s', 'post author', 'test' ),
+        '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+    );
 
-	echo '<span class="byline"> ' . $byline . '</span><span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
-
+    echo '<span class="byline"> ' . $byline . '</span><span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 }
 endif;
 
