b10f28861b26643fb9d98d71f951076bacbb6f80
lyudmila-schastlyvets <zorialv@gmail.com>
2017-03-15 10:38:09 +0200

    Initial blog page commit

--- wp-content/themes/dwellings/inc/template-tags.php
+++ wp-content/themes/dwellings/inc/template-tags.php
@@ -25,16 +25,16 @@ function dwellings_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'dwellings' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		esc_html_x( '%s', 'post date', 'dwellings' ),
+		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . date( "j F, Y", strtotime(strip_tags($time_string)) ). '</a>'
 	);
 
 	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'dwellings' ),
+		esc_html_x( 'By %s', 'post author', 'dwellings' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="byline"> ' . $byline . '</span><span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
 }
 endif;
