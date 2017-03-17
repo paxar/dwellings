a4815adf0fde00a43207209aa514708b81135ebb
lyudmila-schastlyvets <zorialv@gmail.com>
2017-03-17 02:34:08 +0200

    Add changes for single.php

--- wp-content/themes/dwellings/single.php
+++ wp-content/themes/dwellings/single.php
@@ -8,28 +8,21 @@
  */
 
 get_header(); ?>
-    <main id="content" class="site-content">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
+	<main id="content" class="site-content">
+		<div class="container">
+			<section class="blog-page-section">
 
-			get_template_part( 'template-parts/content', get_post_format() );
+				<?php
+				while ( have_posts() ) : the_post();
 
-			the_post_navigation();
+					get_template_part( 'template-parts/content-single', get_post_format() );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				endwhile; // End of the loop.
+				?>
+			</section>
+		</div><!-- #primary -->
+	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
