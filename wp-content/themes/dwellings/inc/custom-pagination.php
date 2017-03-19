91914711df6f96e345907c9475dbd58edb884032
lyudmila-schastlyvets <zorialv@gmail.com>
2017-03-19 22:36:58 +0200

    Fixed pagination on Candidade families page

--- wp-content/themes/dwellings/inc/custom-pagination.php
+++ wp-content/themes/dwellings/inc/custom-pagination.php
@@ -117,37 +117,3 @@ echo '<div class="dwelling-pagination"><ul>' . "\n";
         echo '</ul></div>' . "\n";
 
 }
-
-/*Pagination for Projects page*/
-
-// paxar pagination
-function projects_pagination($pages = '', $range = 4)
-{
-    $showitems = ($range * 2)+1;
-
-    global $paged;
-    if(empty($paged)) $paged = 1;
-
-    if($pages == '')
-    {
-        global $wp_query;
-        $pages = $wp_query->max_num_pages;
-        if(!$pages)
-        {
-            $pages = 1;
-        }
-    }
-
-    if(1 != $pages)
-    {
-        echo "<div class=\"dwelling-pagination col-xs-12\"><ul>";
-        for ($i=1; $i <= $pages; $i++)
-        {
-            if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems ))
-            {
-                echo ($paged == $i)? "<li class=\"active\">".$i."</li>":"<li><a href='".get_pagenum_link($i)."' class=\"active2\">".$i."</a></li>";
-            }
-        }
-        echo "</ul>\n";
-    }
-}
