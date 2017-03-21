82ca11b073699faa1a91eef998edd5f226753cd5
Arthur404 <ananarben@gmail.com>
2017-03-21 18:02:58 +0200

    about us section finish

--- wp-content/themes/dwellings/inc/customizer.php
+++ wp-content/themes/dwellings/inc/customizer.php
@@ -483,6 +483,17 @@ function dwellings_customize_register( $wp_customize ) {
         )
     );
     $wp_customize->add_setting(
+        'description_abut_us'
+    );
+    $wp_customize->add_control(
+        'description_abut_us',
+        array(
+            'label' => esc_html__('Description about us', 'dwellings'),
+            'section' => 'section-about-us',
+            'type' => 'textarea'
+        )
+    );
+    $wp_customize->add_setting(
         'title_testimonials'
     );
     $wp_customize->add_control(
@@ -505,7 +516,6 @@ function dwellings_customize_register( $wp_customize ) {
             )
         )
     );
-
 }
 add_action( 'customize_register', 'dwellings_customize_register' );
 
--- wp-content/themes/dwellings/template-parts/post/content-about-us.php
+++ wp-content/themes/dwellings/template-parts/post/content-about-us.php
@@ -7,19 +7,17 @@
 
 <section class="about-us">
     <div class="container">
-        <div class="about-content">
-            <div class="row">
-                <div class="col-sm-6">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="about-content">
                     <h2 class="title title-decor"><?php echo get_theme_mod('title_abut_us'); ?></h2>
                     <div class="about-content">
-                        <p>Dwellings is a community of charity conscious businesses that want to make a difference by sharing with those in need of housing. Donations are made via products, discounts, services, and financial support.
-                        </p>
-                        <p>Your involvement will make a lasting difference in the lives of those who live in difficult living situations. By making a donation or becoming a volunteer you will help provide a new start for a grateful family.</p>
+                        <p><?php echo get_theme_mod('description_abut_us') ?></p>
                     </div>
                 </div>
-                <div class="about-cover">
-                    <img src="<?php echo get_theme_mod('about-cover-image') ?>" alt="<?php echo get_theme_mod('about-cover-image') ?>">
-                </div>
+            </div>
+            <div class="about-wrap-img">
+                <img src="<?php echo get_theme_mod('about-cover-image') ?>" alt="<?php echo get_theme_mod('about-cover-image') ?>">
             </div>
         </div>
     </div>
