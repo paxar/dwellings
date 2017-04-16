2cde286ca71f11f18e7530f7cd0a51da3b1c7e80
paxar <paxar81@gmail.com>
2017-04-16 19:29:39 +0300

    Add login profile page

--- wp-content/themes/dwellings/header.php
+++ wp-content/themes/dwellings/header.php
@@ -15,37 +15,52 @@ session_start();  /*Starting session for send variables   Added paxar*/
 
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
 
-<?php wp_head(); ?>
-</head>
+		<?php wp_head(); ?>
+    </head>
 
-<body <?php body_class(); ?>>
-<!--<nav id="page" class="site">-->
-	<!--<a class="skip-link screen-reader-text" href="#content"><?php /*esc_html_e( 'Skip to content', 'dwellings' ); */?></a>-->
-    <div id="container">
-    <header id="masthead" class="site-header header-page" role="banner">
-        <div class="header-wrap container">
+    <body <?php body_class(); ?>>
+        <?php
+        global $user_ID, $user_identity;
 
-            <h1 class="logo">
-                <?php the_custom_logo(); ?>
-                Dwellings
-            </h1>
-            <nav class="main-nav">
-                <button class="nav-btn open-btn" id="open-nav">
-                    <i class="fa fa-bars"></i>
-                </button>
-                <button class="nav-btn close-btn remove-btn" id="close-nav">
-                    <i class="fa fa-times"></i>
-                </button>
-                <a href="<?php echo get_permalink(get_theme_mod('url_login')); ?>" class="login sign-in"><i class="fa fa-user"></i>
-                <a href="<?php echo wp_logout_url(home_url()); ?>" class="login sign-out"><i class="fa fa-sign-out"></i>
-                </a>
-                <?php wp_nav_menu(array('them_location' => 'menu-1', 'container' => false, 'menu_class' => 'navigation')) ?>
-        </div>
+        ?>
+        <!--<nav id="page" class="site">-->
+        <!--<a class="skip-link screen-reader-text" href="#content"><?php /*esc_html_e( 'Skip to content', 'dwellings' ); */ ?></a>-->
+        <div id="container">
+            <header id="masthead" class="site-header header-page" role="banner">
+                <div class="header-wrap container">
 
-    </header><!-- #masthead -->
-    </div><!-- .container -->
+                    <h1 class="logo">
+						<?php the_custom_logo(); ?>
+                        Dwellings
+                    </h1>
+                    <nav class="main-nav">
+                        <button class="nav-btn open-btn" id="open-nav">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                        <button class="nav-btn close-btn remove-btn" id="close-nav">
+                            <i class="fa fa-times"></i>
+                        </button>
+                        <a href="<?php echo get_permalink( get_theme_mod( 'url_login' ) ); ?>" class="login sign-in"><i
+                                    class="fa fa-user"></i></a>
+                        <div class="login-wrap">
+                            <a href="<?php echo home_url() . '/charitable-profile/'  ?>" class="login username"><?php echo $user_identity; ?> </a>
+                            <a href="<?php echo wp_logout_url( home_url() ); ?>" class="login sign-out"><i
+                                        class="fa fa-sign-out"></i></a>
+
+                        </div>
+
+
+						<?php wp_nav_menu( array(
+							'them_location' => 'menu-1',
+							'container'     => false,
+							'menu_class'    => 'navigation'
+						) ) ?>
+                </div>
+
+            </header><!-- #masthead -->
+        </div><!-- .container -->
